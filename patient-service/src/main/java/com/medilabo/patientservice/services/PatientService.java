988238b789efb52960652826852f6de2cb0232a3
abrugeilles <abrugeilles@ardans.fr>
2025-10-17 11:52:29 +0200

    patient-service, writing the PatientService layer with basic methods

---
+++ patient-service/src/main/java/com/medilabo/patientservice/services/PatientService.java
@@ -0,0 +1,34 @@
+package com.medilabo.patientservice.services;
+
+import com.medilabo.patientservice.entities.Patient;
+import com.medilabo.patientservice.repositories.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PatientService {
+
+    private final PatientRepository patientRepository;
+
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Optional<Patient> getPatientById(Integer id) {
+        return patientRepository.findById(id);
+    }
+
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
+
+    public void deletePatient(Integer id) {
+        patientRepository.deleteById(id);
+    }
+}
