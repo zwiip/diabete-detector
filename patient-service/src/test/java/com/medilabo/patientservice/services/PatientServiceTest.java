1d0d0b58b3db6dbe3d8200539ce554fe37881c31
abrugeilles <abrugeilles@ardans.fr>
2025-10-29 16:35:10 +0100

    patient-service, writing the tests to cover the searchPatients' logic

--- patient-service/src/test/java/com/medilabo/patientservice/services/PatientServiceTest.java
+++ patient-service/src/test/java/com/medilabo/patientservice/services/PatientServiceTest.java
@@ -13,9 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -27,6 +25,7 @@ class PatientServiceTest {
     private Patient patientBorderline;
     private Patient patientInDanger;
     private Patient patientEarlyOnset;
+    List<Patient> patients = new ArrayList<>();
 
     @Mock
     private PatientRepository patientRepository;
@@ -40,28 +39,74 @@ class PatientServiceTest {
         patientInDanger = new Patient("TestInDanger", "Test", LocalDate.of(2004, 06, 18), Patient.Gender.MALE, "3 Club Road", "300-444-5555");
         patientEarlyOnset = new Patient("TestEarlyOnset", "Test", LocalDate.of(2002, 06, 28), Patient.Gender.FEMALE, "4 Valley Dr", "400-555-6666");
 
-    }
-
-    @Test
-    void givenFourPatients_whenGetAllPatients_shouldReturnFourPatients() {
-        List<Patient> patients = new ArrayList<>();
         patients.add(patientNone);
         patients.add(patientBorderline);
         patients.add(patientInDanger);
         patients.add(patientEarlyOnset);
-        when(patientRepository.findAll()).thenReturn(patients);
+    }
 
+    @Test
+    void givenFourPatients_whenGetAllPatients_shouldReturnFourPatients() {
+        when(patientRepository.findAll()).thenReturn(patients);
         assertEquals(4, patientService.getAllPatients().size());
     }
 
     @Test
     void givenAnExistingID_whenGetPatientById_shouldReturnTheCorrectPatient() {
-        when(patientRepository.findById(anyInt())).thenReturn(Optional.ofNullable(patientNone));
+        when(patientRepository.findById(1)).thenReturn(Optional.ofNullable(patientNone));
 
         Optional<Patient> patient = patientService.getPatientById(1);
 
-        assertNotNull(patient);
+        assertTrue(patient.isPresent());
         assertEquals("TestNone", patient.get().getName());
     }
 
+    @Test
+    void givenPartialName_whenSearchPatients_shouldReturnTwoPatients() {
+        when(patientRepository.findAll()).thenReturn(patients);
+        List<Patient> patientsWithIN = patientService.searchPatients("in", null, null, null);
+        assertEquals(2, patientsWithIN.size());
+    }
+
+    @Test
+    void givenAFullName_whenSearchPatients_shouldReturnTheMatchingPatient() {
+        when(patientRepository.findAll()).thenReturn(patients);
+        List<Patient> patientNamedTestBorderline = patientService.searchPatients("TestBorderline", null, null, null);
+        assertEquals(1, patientNamedTestBorderline.size());
+    }
+
+    @Test
+    void givenPartialFirstName_whenSearchPatients_shouldReturnTheMatchingPatients() {
+        when(patientRepository.findAll()).thenReturn(patients);
+        List<Patient> patientWithTE = patientService.searchPatients(null, "Te", null, null);
+        assertEquals(4, patientWithTE.size());
+    }
+
+    @Test
+    void givenFirstName_whenSearchPatients_shouldReturnTheMatchingPatients() {
+        when(patientRepository.findAll()).thenReturn(patients);
+        List<Patient> patientNamedTest = patientService.searchPatients(null, "Test", null, null);
+        assertEquals(4, patientNamedTest.size());
+    }
+
+    @Test
+    void givenFemaleGender_whenSearchPatients_thenReturnMatchingPatients() {
+        when(patientRepository.findAll()).thenReturn(patients);
+        List<Patient> femalePatients = patientService.searchPatients(null, null, null, Patient.Gender.FEMALE);
+        assertEquals(2, femalePatients.size());
+    }
+
+    @Test
+    void givenAMatchingDate_whenSearchPatients_thenReturnMatchingPatient() {
+        when(patientRepository.findAll()).thenReturn(patients);
+        List<Patient> patient = patientService.searchPatients(null, null, LocalDate.of(2004, 06, 18), null);
+        assertEquals(1, patient.size());
+    }
+
+    @Test
+    void givenNoCriteria_whenSearchPatients_thenReturnAllPatients() {
+        when(patientRepository.findAll()).thenReturn(patients);
+        List<Patient> resultWithoutFilter = patientService.searchPatients(null, null, null, null);
+        assertEquals(4, resultWithoutFilter.size());
+    }
 }
