f4aae61b142d2b29efdb004ea75903360a28ad12
abrugeilles <abrugeilles@ardans.fr>
2025-12-31 18:23:20 +0100

    patient-service, ajout configuration bdd

---
+++ patient-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO patients (name, first_name, birth_date, gender, address, phone)
+VALUES ('TestNone', 'Test', '1966-12-31', 'FEMALE', '1 Brookside St', '100-222-3333'),
+       ('TestBorderline', 'Test', '1945-06-24', 'MALE', '2 High St', '200-333-4444'),
+       ('TestInDanger', 'Test', '2004-06-18', 'MALE', '3 Club Road', '300-444-5555'),
+       ('TestEarlyOnset', 'Test', '2002-06-28', 'FEMALE', '4 Valley Dr', '400-555-6666');
