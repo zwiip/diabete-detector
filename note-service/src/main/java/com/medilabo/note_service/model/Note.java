18a96fcf07f6bd8a600e3712f97bea79472e0f90
abrugeilles <abrugeilles@ardans.fr>
2026-01-28 10:32:14 +0100

    Note-service, implémentation du modèle "note"

---
+++ note-service/src/main/java/com/medilabo/note_service/model/Note.java
@@ -0,0 +1,46 @@
+package com.medilabo.note_service.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "notes")
+public class Note {
+
+    @Id
+    private String id;
+    private Integer patientId;
+    private String noteText;
+    private LocalDateTime date;
+
+    public Note(Integer patientId, String noteText, LocalDateTime date) {
+        this.patientId = patientId;
+        this.noteText = noteText;
+        this.date = date;
+    }
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String note) {
+        this.noteText = note;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
