20b5f24c706d3715200cf9f0f3332a20936c4805
abrugeilles <abrugeilles@ardans.fr>
2026-01-28 11:07:12 +0100

    Note-service, Couche controller + DTO (test postman ok pour post et get)

---
+++ note-service/src/main/java/com/medilabo/note_service/controller/NoteController.java
@@ -0,0 +1,32 @@
+package com.medilabo.note_service.controller;
+
+import com.medilabo.note_service.dto.NoteCreateDTO;
+import com.medilabo.note_service.model.Note;
+import com.medilabo.note_service.service.NoteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notes")
+public class NoteController {
+
+    private final NoteService noteService;
+
+    public NoteController(NoteService noteService) {
+        this.noteService = noteService;
+    }
+
+    @GetMapping
+    public List<Note> getNotes(@RequestParam Integer patientId) {
+        return noteService.getNotesByPatientId(patientId);
+    }
+
+    @PostMapping
+    public Note addNote(@RequestBody NoteCreateDTO newNote) {
+        return noteService.addNote(
+                newNote.getPatientId(),
+                newNote.getNoteText()
+        );
+    }
+}
---
+++ note-service/src/main/java/com/medilabo/note_service/dto/NoteCreateDTO.java
@@ -0,0 +1,23 @@
+package com.medilabo.note_service.dto;
+
+public class NoteCreateDTO {
+
+    private Integer patientId;
+    private String noteText;
+
+    public Integer getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Integer patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+}
