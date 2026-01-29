d54e8bccd045eea7cae6cba4ff0a90674958e5d8
abrugeilles <abrugeilles@ardans.fr>
2026-01-29 09:35:53 +0100

    Assessment-Service, création du microservice

---
+++ assessment-service/src/main/java/com/medilabo/assessment_service/AssessmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.assessment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentServiceApplication.class, args);
+	}
+
+}
