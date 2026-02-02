ca4553ec6e66377c4a980507f88ef592f8f46284
abrugeilles <abrugeilles@ardans.fr>
2026-02-02 16:00:33 +0100

    Note-Service, Corrections des données

--- note-service/src/main/java/com/medilabo/note_service/config/DataInitializer.java
+++ note-service/src/main/java/com/medilabo/note_service/config/DataInitializer.java
@@ -22,35 +22,35 @@ public class DataInitializer {
 
             List<Note> notes = List.of(
                     new Note(1,
-                            "Le patient déclare qu'il se sent très bien.\nPoids égal ou inférieur au poids recommandé.",
+                            "Le patient déclare qu'il 'se sent très bien' Poids égal ou inférieur au poids recommandé",
                             LocalDateTime.of(2025, 1, 1, 10, 0)
                     ),
                     new Note(2,
-                            "Le patient déclare qu'il ressent beaucoup de stress au travail.\nIl se plaint également que son audition est anormale dernièrement.",
+                            "Le patient déclare qu'il ressent beaucoup de stress au travail Il se plaint également que son audition est anormale dernièrement",
                             LocalDateTime.of(2025, 1, 2, 11, 30)
                     ),
                     new Note(2,
-                            "Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois.\nIl remarque également que son audition continue d'être anormale.",
+                            "Le patient déclare avoir fait une réaction aux médicaments au cours des 3 derniers mois Il remarque également que son audition continue d'être anormale",
                             LocalDateTime.of(2025, 2, 15, 9, 15)
                     ),
                     new Note(3,
-                            "Le patient déclare qu'il fume depuis peu.",
+                            "Le patient déclare qu'il fume depuis peu",
                             LocalDateTime.of(2025, 3, 10, 14, 0)
                     ),
                     new Note(3,
-                            "Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière.\nIl se plaint également de crises d’apnée respiratoire anormales.\nTests de laboratoire indiquant un taux de cholestérol LDL élevé.",
+                            "Le patient déclare qu'il est fumeur et qu'il a cessé de fumer l'année dernière Il se plaint également de crises d’apnée respiratoire anormales Tests de laboratoire indiquant un taux de cholestérol LDL élevé",
                             LocalDateTime.of(2025, 4, 5, 16, 45)
                     ),
                     new Note(4,
-                            "Le patient déclare qu'il lui est devenu difficile de monter les escaliers.\nIl se plaint également d’être essoufflé.\nTests de laboratoire indiquant que les anticorps sont élevés.\nRéaction aux médicaments.",
+                            "Le patient déclare qu'il lui est devenu difficile de monter les escaliers Il se plaint également d’être essoufflé Tests de laboratoire indiquant que les anticorps sont élevés Réaction aux médicaments",
                             LocalDateTime.of(2025, 5, 20, 10, 30)
                     ),
                     new Note(4,
-                            "Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps.",
+                            "Le patient déclare qu'il a mal au dos lorsqu'il reste assis pendant longtemps",
                             LocalDateTime.of(2025, 6, 18, 9, 0)
                     ),
                     new Note(4,
-                            "Le patient déclare avoir commencé à fumer depuis peu.\nHémoglobine A1C supérieure au niveau recommandé.",
+                            "Le patient déclare avoir commencé à fumer depuis peu Hémoglobine A1C supérieure au niveau recommandé",
                             LocalDateTime.of(2025, 7, 22, 15, 20)
                     ),
                     new Note(4,
