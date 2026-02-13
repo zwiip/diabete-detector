ace38f1cca14860508a886b80056cef3b7449df4
abrugeilles <abrugeilles@ardans.fr>
2026-02-13 11:08:22 +0100

    patient-service, ajout database mySql et corrections

--- patient-service/src/test/java/com/medilabo/patientservice/repositories/PatientRepositoryTest.java
+++ patient-service/src/test/java/com/medilabo/patientservice/repositories/PatientRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
 class PatientRepositoryTest {
 
     @Autowired
